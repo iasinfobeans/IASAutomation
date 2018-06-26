3220421b322c7a47247b048d64949e4857121819
guptashefali <shefali.27g@gmail.com>
2018-06-26 17:54:48 +0530

    log4j added

--- IAS/src/main/java/com/ias/setup/Driver.java
+++ IAS/src/main/java/com/ias/setup/Driver.java
@@ -2,21 +2,23 @@ package com.ias.setup;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.annotations.AfterMethod;
 
 import com.ias.util.Prop;
 
 public class Driver {
 	
 	public static WebDriver driver;
+	private static Logger log = Logger.getLogger(Driver.class.getName());
 
 	public static WebDriver getDriver(String browserName) throws IOException {
 		if (Prop.fetchPropertyValue("browser").toString().equalsIgnoreCase("chrome")) {
 			System.setProperty("webdriver.chrome.driver", ".\\src\\main\\resources\\driver\\chromedriver.exe");
+			log.info("Browser used is: "+ browserName);
 			return new ChromeDriver();
 
 		} else if (browserName.equalsIgnoreCase("IE")) {
--- IAS/src/main/java/com/ias/util/Prop.java
+++ IAS/src/main/java/com/ias/util/Prop.java
@@ -5,13 +5,20 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+
+import com.ias.setup.Driver;
+
 public class Prop {
+	
+	private static Logger log = Logger.getLogger(Prop.class.getName());
 
 	public static String fetchPropertyValue(String key) throws IOException {
 		FileInputStream fis = new FileInputStream(
 				System.getProperty("user.dir")+ "\\src\\main\\resources\\properties\\config.properties");
 		Properties property = new Properties();
 		property.load(fis);
+		log.info("config file loaded");
 		return property.get(key).toString();
 	}
 	
@@ -20,6 +27,7 @@ public class Prop {
 				System.getProperty("user.dir")+ "\\src\\main\\resources\\properties\\testData.properties");
 		Properties property = new Properties();
 		property.load(fis);
+		log.info("test data file loaded");
 		return property.get(key).toString();
 	}
 }
