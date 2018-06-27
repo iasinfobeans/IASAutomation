2c392920bfab7aa9aa5e3dc17ee089b80dfbfc8e
Nisha.Gathiya <Nisha.Gathiya@IB-SEZ-PC-062.infobeans.in>
2018-06-27 15:26:16 +0530

    Handled exception

--- IAS/src/main/java/com/ias/util/Prop.java
+++ IAS/src/main/java/com/ias/util/Prop.java
@@ -10,24 +10,40 @@ import org.apache.log4j.Logger;
 import com.ias.setup.Driver;
 
 public class Prop {
-	
+
 	private static Logger log = Logger.getLogger(Prop.class.getName());
 
-	public static String fetchPropertyValue(String key) throws IOException {
-		FileInputStream fis = new FileInputStream(
-				System.getProperty("user.dir")+ "\\src\\main\\resources\\properties\\config.properties");
-		Properties property = new Properties();
-		property.load(fis);
-		log.info("config file loaded");
+	public static String getConfigValue(String key) throws IOException {
+		FileInputStream fis;
+		Properties property =null;
+		try {
+			fis = new FileInputStream(
+					System.getProperty("user.dir")+ "\\src\\main\\resources\\properties\\config.properties");
+			property = new Properties();
+			property.load(fis);
+			log.info("config file loaded");
+		}catch(FileNotFoundException e) {
+			log.info("Failed to load testdata file");
+		} catch (IOException e) {
+			log.info("Faied to open testdata file");	
+		}
 		return property.get(key).toString();
 	}
-	
-	public static String fetchTestData(String key) throws IOException {
-		FileInputStream fis = new FileInputStream(
-				System.getProperty("user.dir")+ "\\src\\main\\resources\\properties\\testData.properties");
-		Properties property = new Properties();
-		property.load(fis);
-		log.info("test data file loaded");
+
+	public static String getTestData(String key) {
+		FileInputStream fis;
+		Properties property =null;
+		try {
+			fis = new FileInputStream(
+					System.getProperty("user.dir")+ "\\src\\main\\resources\\properties\\testData.properties");
+			property = new Properties();
+			property.load(fis);
+			log.info("test data file loaded");
+		}catch(FileNotFoundException e) {
+			log.info("Failed to load testdata file");
+		} catch (IOException e) {
+			log.info("Faied to open testdata file");	
+		}
 		return property.get(key).toString();
 	}
 }
